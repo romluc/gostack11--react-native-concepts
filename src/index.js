e0098329d39c5da8cd804ed54aecf59ac86bf08c
romluc <romulofilhofilho@gmail.com>
2020-05-02 15:49:16 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return <View style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#008b8b',
+  },
+});
