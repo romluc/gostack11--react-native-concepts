10751014ae118c0abc7c2cf48f9c8471db0249cd
romluc <romulofilhofilho@gmail.com>
2020-05-02 16:51:03 -0300

    Create add route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: `Romulo Castro`,
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle='light-content' />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello maaans!</Text>
-      </View>
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Add Project</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </>
   );
 }
@@ -16,13 +57,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#007788',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 
-  title: {
-    color: '#F5F5F5',
-    fontSize: 32,
+  project: {
+    color: '#F0F0F0',
+    fontSize: 30,
+    marginHorizontal: 20,
+  },
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 24,
+  },
+  buttonText: {
+    color: '#333',
+    fontSize: 24,
     fontWeight: 'bold',
   },
 });
